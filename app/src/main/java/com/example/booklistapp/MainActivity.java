5bb31bd664db80acf3c23977147b0c83d67b6c65
RishiMatura <rishimatura27@gmail.com>
2024-02-03 01:19:34 +0530

    changed the size of text fields, for author name to match screen Size

--- app/src/main/java/com/example/booklistapp/MainActivity.java
+++ app/src/main/java/com/example/booklistapp/MainActivity.java
@@ -20,24 +20,25 @@ public class MainActivity extends AppCompatActivity {
         RecyclerView recyclerView = findViewById(R.id.BookView);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
-        arrBook.add(new BooksModel(R.drawable.acceptance, "Acceptance", "Jeff VanderMeer"));
-        arrBook.add(new BooksModel(R.drawable.authority, "Authority", "Jeff VanderMeer"));
-        arrBook.add(new BooksModel(R.drawable.book_of_strange_new_things, "The Book of Strange New Things", "Michel Faber"));
-        arrBook.add(new BooksModel(R.drawable.closing_time, "Closing Time", "Joe Queenan"));
-        arrBook.add(new BooksModel(R.drawable.dangerous_frames, "Dangerous Frames", "Nicholas J. G. Winter"));
-        arrBook.add(new BooksModel(R.drawable.harry_potter, "Harry Potter and the Deathly Hallows", "J.K Rowling"));
-        arrBook.add(new BooksModel(R.drawable.how_to_live, "How to Live", "Sarah Bakewell"));
-        arrBook.add(new BooksModel(R.drawable.indecision_2, "Indecision", "Benjamin Kunkel"));
-        arrBook.add(new BooksModel(R.drawable.lost_decades, "Lost Decades: The Making of America's Debt Crisis and the Long Recovery", "Jeffry Frieden and Menzie Chinn"));
-        arrBook.add(new BooksModel(R.drawable.no_longer_human, "No Longer Human", "Osamu Dazai"));
-        arrBook.add(new BooksModel(R.drawable.nova, "Nova", "Samuel R. Delany"));
-        arrBook.add(new BooksModel(R.drawable.panic, "Panic: The Story of Modern Financial Insanity", "Michael Lewis"));
-        arrBook.add(new BooksModel(R.drawable.the_last_skin, "The Last Skin", "Barbara Ras"));
-        arrBook.add(new BooksModel(R.drawable.the_psychopath_test, "The Psychopath Test", "Jon Ronson"));
-        arrBook.add(new BooksModel(R.drawable.the_spys_son, "The Spy's Son", "Bryan Denson"));
-        arrBook.add(new BooksModel(R.drawable.wall_street, "Wall Street: America's Dream Palace", "Steve Fraser"));
-        arrBook.add(new BooksModel(R.drawable.you_shall_know_them, "You Shall Know Them", "Vercors"));
+        arrBook.add(new BooksModel(R.drawable.acceptance, "Acceptance", "By: Jeff VanderMeer"));
+        arrBook.add(new BooksModel(R.drawable.authority, "Authority", "By: Jeff VanderMeer"));
+        arrBook.add(new BooksModel(R.drawable.book_of_strange_new_things, "The Book of Strange New Things", "By: Michel Faber"));
+        arrBook.add(new BooksModel(R.drawable.closing_time, "Closing Time", "By: Joe Queenan"));
+        arrBook.add(new BooksModel(R.drawable.dangerous_frames, "Dangerous Frames", "By: Nicholas J. G. Winter"));
+        arrBook.add(new BooksModel(R.drawable.harry_potter, "Harry Potter and the Deathly Hallows", "By: J.K Rowling"));
+        arrBook.add(new BooksModel(R.drawable.how_to_live, "How to Live", "By: Sarah Bakewell"));
+        arrBook.add(new BooksModel(R.drawable.indecision_2, "Indecision", "By: Benjamin Kunkel"));
+        arrBook.add(new BooksModel(R.drawable.lost_decades, "Lost Decades: The Making of America's Debt Crisis and the Long Recovery", "By: Jeffry Frieden and Menzie Chinn"));
+        arrBook.add(new BooksModel(R.drawable.no_longer_human, "No Longer Human", "By: Osamu Dazai"));
+        arrBook.add(new BooksModel(R.drawable.nova, "Nova", "By: Samuel R. Delany"));
+        arrBook.add(new BooksModel(R.drawable.panic, "Panic: The Story of Modern Financial Insanity", "By: Michael Lewis"));
+        arrBook.add(new BooksModel(R.drawable.the_last_skin, "The Last Skin", "By: Barbara Ras"));
+        arrBook.add(new BooksModel(R.drawable.the_psychopath_test, "The Psychopath Test", "By: Jon Ronson"));
+        arrBook.add(new BooksModel(R.drawable.the_spys_son, "The Spy's Son", "By: Bryan Denson"));
+        arrBook.add(new BooksModel(R.drawable.wall_street, "Wall Street: America's Dream Palace", "By: Steve Fraser"));
+        arrBook.add(new BooksModel(R.drawable.you_shall_know_them, "You Shall Know Them", "By: Vercors"));
 
-
+        RecyclerBookAdapter adapter = new RecyclerBookAdapter(this, arrBook);
+        recyclerView.setAdapter(adapter);
     }
 }
--- app/src/main/java/com/example/booklistapp/RecyclerBookAdapter.java
+++ app/src/main/java/com/example/booklistapp/RecyclerBookAdapter.java
@@ -4,15 +4,20 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
+
 public class RecyclerBookAdapter extends RecyclerView.Adapter<RecyclerBookAdapter.ViewHolder> {
     Context context;
-
-    public RecyclerBookAdapter(Context context) {
+    ArrayList<BooksModel> booksModelArrayList;
+    public RecyclerBookAdapter(Context context, ArrayList<BooksModel> booksModelArrayList) {
         this.context = context;
+        this.booksModelArrayList = booksModelArrayList;
     }
 
     @NonNull
@@ -25,17 +30,24 @@ public class RecyclerBookAdapter extends RecyclerView.Adapter<RecyclerBookAdapte
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-
+        holder.bookPhoto.setImageResource(booksModelArrayList.get(position).img);
+        holder.bookName.setText(booksModelArrayList.get(position).book_name);
+        holder.authorName.setText(booksModelArrayList.get(position).author_name);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return booksModelArrayList.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
+        ImageView bookPhoto;
+        TextView bookName,authorName;
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+            bookPhoto = itemView.findViewById(R.id.book_image);
+            bookName = itemView.findViewById(R.id.book_name);
+            authorName = itemView.findViewById(R.id.author_name);
         }
     }
 }
